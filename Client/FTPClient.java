fb6cbee54355c54f57cd5743d5d7ad93431b4eaf
Srikanth Bharadwaj <bharadwajs@ufl.edu>
2019-10-09 10:20:17 -0400

    Initializing socket and i/o streams. Enabling server start and establishing client-server connections. Closing connections at the end of operations.

--- Client/FTPClient.java
+++ Client/FTPClient.java
@@ -1,3 +1,46 @@
+import java.io.*;
+import java.net.*;
+
 public class FTPClient {
 
+    private int SERVER_PORT = 5000;
+    private String SERVER_PROXY = "localhost";
+
+    private Socket socket = null;
+    private DataOutputStream out;
+    private DataInputStream in;
+
+    public FTPClient() {}
+
+    private void run() {
+        try {
+            socket = new Socket(SERVER_PROXY, SERVER_PORT);
+            System.out.println("Connected");
+            out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+            out.flush();
+            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+        }
+        catch(UnknownHostException u) {
+            System.out.println(u);
+        }
+        catch(IOException i) {
+            System.out.println(i);
+        }
+        finally {
+            System.out.println("Closing connection");
+            try {
+                in.close();
+                out.close();
+                socket.close();
+            }
+            catch(IOException ex) {
+                System.out.println(ex);
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        FTPClient cli = new FTPClient();
+        cli.run();
+    }
 }
--- Server/FTPServer.java
+++ Server/FTPServer.java
@@ -1,3 +1,56 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class FTPServer {
 
+    private int SERVER_PORT = 5000;
+
+    private ServerSocket server = null;
+    private Socket socket = null;
+    private DataOutputStream out;
+    private DataInputStream in;
+
+    public FTPServer() {}
+
+    private void run() {
+        try {
+            server = new ServerSocket(SERVER_PORT);
+            System.out.println("Server started");
+        }
+        catch(IOException ex) {
+            System.out.println(ex);
+        }
+        System.out.println("Waiting for a client ...");
+        try {
+            socket = server.accept();
+            System.out.println("Client accepted");
+            out = new DataOutputStream(
+                    new BufferedOutputStream(socket.getOutputStream())
+            );
+            out.flush();
+            in = new DataInputStream(
+                    new BufferedInputStream(socket.getInputStream())
+            );
+        }
+        catch(IOException ex) {
+            System.out.println(ex);
+        }
+        finally {
+            System.out.println("Closing connection");
+            try {
+                in.close();
+                out.close();
+                server.close();
+            }
+            catch(IOException ex) {
+                System.out.println(ex);
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        FTPServer serv = new FTPServer();
+        serv.run();
+    }
 }
