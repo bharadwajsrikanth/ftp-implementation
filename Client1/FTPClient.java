368cd0d1ed01fe883444ce47a92607371f477ff6
Srikanth Bharadwaj <bharadwajs@ufl.edu>
2019-10-13 11:43:33 -0400

    Allow multiple client connections to the server

---
+++ Client1/FTPClient.java
@@ -0,0 +1,169 @@
+import java.io.*;
+import java.net.*;
+import java.util.StringTokenizer;
+
+public class FTPClient {
+
+    private int SERVER_PORT = 5000;
+    private String SERVER_PROXY = "localhost";
+
+    private Socket socket = null;
+    private BufferedReader bufferedReader;
+    private DataOutputStream out;
+    private DataInputStream in;
+    private ObjectInputStream in_obj;
+    private String auth_msg = "";
+
+    public FTPClient() {}
+
+    private void sendMessage(String msg) {
+        try {
+            out.writeUTF(msg);
+            out.flush();
+        }
+        catch(IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    private void getAuthCredentials(BufferedReader bufferedReader) {
+        String server_response = "";
+        String username, password;
+        try {
+            System.out.print("Enter username: ");
+            username = bufferedReader.readLine();
+            sendMessage(username);
+            System.out.print("Enter password: ");
+            password = bufferedReader.readLine();
+            sendMessage(password);
+        }
+        catch(IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    private void receiveFile(String name) {
+        //System.out.println("File name: " + name);
+        try {
+            FileOutputStream fos = new FileOutputStream(name);
+            long filesize = in.readLong();
+            fos.flush();
+            byte[] bytes = new byte[8192];
+            int count;
+            while (filesize>0 && (count = in.read(bytes,0,(int)Math.min(bytes.length,filesize))) != -1) {
+                fos.write(bytes, 0, count);
+                filesize -= count;
+            }
+            fos.close();
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Error occurred while storing the file.");
+        }
+        catch(IOException e){
+            System.out.println("Exception while taking inputs/writing file");
+        }
+    }
+
+    private void uploadFile(String name) {
+        System.out.println("Uploading file: " + name);
+        File file = new File(name);
+        long length = file.length();
+
+        byte bytes[] = new byte[8192];
+        try {
+            System.out.println("size: "+length);
+            out.flush();
+            out.writeLong(length);
+            InputStream filein = new FileInputStream(file);
+            int count;
+            while ((count = filein.read(bytes)) > 0) {
+                out.write(bytes, 0, count);
+            }
+            out.flush();
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("File not found on the server.");
+        }
+        catch(IOException e) {
+            System.out.println("Exception while taking inputs/reading file");
+        }
+        System.out.println("Uploaded file: " + name);
+    }
+
+    private void run() {
+        try {
+            socket = new Socket(SERVER_PROXY, SERVER_PORT);
+            System.out.println("Connected");
+            out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+            out.flush();
+            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            getAuthCredentials(bufferedReader);
+            in_obj = new ObjectInputStream(socket.getInputStream());
+            auth_msg = (String) in_obj.readObject();
+            System.out.println(auth_msg);
+
+            if (auth_msg.charAt(0) == 'W') {
+                boolean exec = true;
+                String command = "";
+                String operation = "";
+                StringTokenizer st;
+                while (exec) {
+                    System.out.print("Enter command: ");
+                    command = bufferedReader.readLine();
+                    st = new StringTokenizer(command);
+                    sendMessage(command);
+                    operation = st.nextToken();
+                    switch (operation.charAt(0)) {
+                        case 'g':
+                        case 'G':
+                            receiveFile(st.nextToken());
+                            break;
+                        case 'd':
+                        case 'D':
+                            String files = (String) in_obj.readObject();
+                            System.out.println("Files available on server: ");
+                            System.out.println(files);
+                            break;
+                        case 'u':
+                        case 'U':
+                            uploadFile(st.nextToken());
+                            break;
+                        case 'e':
+                        case 'E':
+                            System.out.println("Exiting program");
+                            exec = false;
+                            break;
+                        default:
+                            System.out.println("Operation not implemented");
+                    }
+                }
+            }
+        }
+        catch(UnknownHostException u) {
+            System.out.println(u);
+        }
+        catch(ClassNotFoundException ex) {
+            System.out.println(ex);
+        }
+        catch(IOException i) {
+            i.printStackTrace();
+        }
+        finally {
+            System.out.println("Closing connection");
+            try {
+                in.close();
+                out.close();
+                socket.close();
+            }
+            catch(IOException ex) {
+                System.out.println(ex);
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        FTPClient cli = new FTPClient();
+        cli.run();
+    }
+}
--- Server/FTPServer.java
+++ Server/FTPServer.java
@@ -11,190 +11,208 @@ public class FTPServer {
 
     private int SERVER_PORT = 5000;
 
-    private ServerSocket server = null;
-    private Socket socket = null;
+    private static ServerSocket server = null;
+    //private Socket socket = null;
     private DataOutputStream out;
     private DataInputStream in;
     private ObjectOutputStream out_obj;
 
-    public FTPServer() {}
+    static int clientNum;
 
-    public String getUsername() {
-        return this.USERNAME;
-    }
-
-    public String getPassword() {
-        return this.PASSWORD;
-    }
-
-    void sendMessage(String msg) {
-        try {
-            out_obj.flush();
-            out_obj.writeObject(msg);
-            out_obj.flush();
-        }
-        catch(IOException i){
-            System.out.println(i);
-        }
-    }
-
-    private void receiveFile(String name) {
-        System.out.println("Receiving File: " + name);
-        try {
-            FileOutputStream fout = new FileOutputStream(name);
-            long filesize = in.readLong();
-            System.out.println("Filesize: "+filesize);
-            fout.flush();
-            byte[] bytes = new byte[8192];
-            int count;
-            while (filesize>0 && (count = in.read(bytes,0,(int)Math.min(bytes.length,filesize))) != -1) {
-                fout.write(bytes, 0, count);
-                filesize -= count;
-            }
-            fout.close();
-        }
-        catch(FileNotFoundException e) {
-            System.out.println("Error occurred while storing the file.");
-        }
-        catch(IOException e) {
-            System.out.println("Exception while taking inputs/writing file");
-        }
-    }
-
-    private void sendFile(String name) {
-        File file = new File(name);
-        long length = file.length();
-        InputStream filein;
-        byte bytearray[] = new byte[8192];
-
-        try {
-            System.out.println("Sending file " + name + " to client");
-            out.flush();
-            out.writeLong(length);
-            filein = new FileInputStream(file);
-            int count;
-            while ((count = filein.read(bytearray)) > 0) {
-                out.write(bytearray, 0, count);
-            }
-            out.flush();
-        }
-        catch(FileNotFoundException e) {
-            System.out.println("File not found on the server.");
-        }
-        catch(IOException e){
-            System.out.println("Exception while taking inputs/reading file");
-        }
-    }
-
-    String getFiles(File directory) {
-        StringBuilder files = new StringBuilder("");
-        for(File fe: directory.listFiles()){
-            //if(!Pattern.matches("*class", fe.getName()) && !Pattern.matches("*java", fe.getName())) {
-                files.append("\t"+fe.getName()+"\n");
-            //}
-        }
-        return files.toString();
-    }
-
-    private void performAuthentication() {
-        String username, password, auth_message;
-        try {
-            username = (String) in.readUTF();
-            password = (String) in.readUTF();
-            if(username.equals(getUsername()) && password.equals(getPassword())) {
-                System.out.println("Authentication successful, sending message to client");
-                auth_message = "Welcome, " + username;
-                sendMessage(auth_message);
-            }
-            else {
-                System.out.println("Authentication failed");
-                auth_message = "Login Failed";
-                sendMessage(auth_message);
-            }
-
-        }
-        catch(IOException i) {
-            System.out.println(i);
-        }
-    }
-
-    private void performOperation() {
-        String command, operation, filename;
-        StringTokenizer st;
-        try {
-            while (true) {
-                command = (String) in.readUTF();
-                st = new StringTokenizer(command);
-                operation = st.nextToken();
-                System.out.println("Requested operation: " + operation);
-                switch (operation.charAt(0)) {
-                    case 'g':
-                    case 'G':
-                        filename = st.nextToken();
-                        sendFile(filename);
-                        break;
-                    case 'd':
-                    case 'D':
-                        File cur_directory = new File(".");
-                        String files = getFiles(cur_directory);
-                        System.out.println("Sending file names to client");
-                        sendMessage(files);
-                        break;
-                    case 'u':
-                    case 'U':
-                        receiveFile(st.nextToken());
-                        break;
-                    default:
-                        System.out.println("Invalid Input");
-                }
-            }
-        }
-        catch(IOException i) {
-            System.out.println(i);
-        }
-    }
-
-    private void run() {
-        try {
-            server = new ServerSocket(SERVER_PORT);
-            System.out.println("Server started");
-        }
-        catch(IOException ex) {
-            System.out.println(ex);
-        }
-        System.out.println("Waiting for a client ...");
-        try {
-            socket = server.accept();
-            System.out.println("Client accepted");
-            out = new DataOutputStream(
-                    new BufferedOutputStream(socket.getOutputStream())
-            );
-            out.flush();
-            in = new DataInputStream(
-                    new BufferedInputStream(socket.getInputStream())
-            );
-            out_obj = new ObjectOutputStream(socket.getOutputStream());
-            performAuthentication();
-            performOperation();
-        }
-        catch(IOException i) {
-            System.out.println(i);
-        }
-        finally {
-            System.out.println("Closing connection");
-            try {
-                in.close();
-                out.close();
-                server.close();
-            }
-            catch(IOException ex) {
-                System.out.println(ex);
-            }
-        }
+    public FTPServer() {
     }
 
     public static void main(String args[]) {
-        FTPServer serv = new FTPServer();
-        serv.run();
+        System.out.println("The server is started");
+        System.out.println("Waiting for a client ...");
+        clientNum = 1;
+        try {
+            server = new ServerSocket(5000);
+            while (true) {
+                new Util(server.accept(), clientNum).start();
+                System.out.println("Client: " + clientNum + " connected!");
+                clientNum++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private static class Util extends Thread {
+        private String USERNAME = "gator";
+        private String PASSWORD = "password";
+
+        private int SERVER_PORT = 5000;
+
+        private Socket socket = null;
+        private DataOutputStream out;
+        private DataInputStream in;
+        private ObjectOutputStream out_obj;
+
+        private int num;
+
+        public Util(Socket socket, int num) {
+            this.socket = socket;
+            this.num = num;
+        }
+
+        public String getUsername() {
+            return this.USERNAME;
+        }
+
+        public String getPassword() {
+            return this.PASSWORD;
+        }
+
+        void sendMessage(String msg) {
+            try {
+                out_obj.flush();
+                out_obj.writeObject(msg);
+                out_obj.flush();
+            } catch (IOException i) {
+                System.out.println(i);
+            }
+        }
+
+        private void receiveFile(String name) {
+            System.out.println("Receiving File: " + name);
+            try {
+                FileOutputStream fout = new FileOutputStream(name);
+                long filesize = in.readLong();
+                System.out.println("Filesize: " + filesize);
+                fout.flush();
+                byte[] bytes = new byte[8192];
+                int count;
+                while (filesize > 0 && (count = in.read(bytes, 0, (int) Math.min(bytes.length, filesize))) != -1) {
+                    fout.write(bytes, 0, count);
+                    filesize -= count;
+                }
+                fout.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("Error occurred while storing the file.");
+            } catch (IOException e) {
+                System.out.println("Exception while taking inputs/writing file");
+            }
+        }
+
+        private void sendFile(String name) {
+            File file = new File(name);
+            long length = file.length();
+            InputStream filein;
+            byte bytearray[] = new byte[8192];
+
+            try {
+                System.out.println("Sending file " + name + " to client");
+                out.flush();
+                out.writeLong(length);
+                filein = new FileInputStream(file);
+                int count;
+                while ((count = filein.read(bytearray)) > 0) {
+                    out.write(bytearray, 0, count);
+                }
+                out.flush();
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found on the server.");
+            } catch (IOException e) {
+                System.out.println("Exception while taking inputs/reading file");
+            }
+        }
+
+        String getFiles(File directory) {
+            StringBuilder files = new StringBuilder("");
+            for (File fe : directory.listFiles()) {
+                //if(!Pattern.matches("*class", fe.getName()) && !Pattern.matches("*java", fe.getName())) {
+                files.append("\t" + fe.getName() + "\n");
+                //}
+            }
+            return files.toString();
+        }
+
+        private void performAuthentication() {
+            String username, password, auth_message;
+            try {
+                username = (String) in.readUTF();
+                password = (String) in.readUTF();
+                if (username.equals(getUsername()) && password.equals(getPassword())) {
+                    System.out.println("Authentication successful, sending message to client");
+                    auth_message = "Welcome, " + username;
+                    sendMessage(auth_message);
+                } else {
+                    System.out.println("Authentication failed");
+                    auth_message = "Login Failed";
+                    sendMessage(auth_message);
+                }
+
+            } catch (IOException i) {
+                System.out.println(i);
+            }
+        }
+
+        private void performOperation() {
+            String command, operation, filename;
+            StringTokenizer st;
+            try {
+                while (true) {
+                    command = (String) in.readUTF();
+                    st = new StringTokenizer(command);
+                    operation = st.nextToken();
+                    System.out.println("Requested operation: " + operation);
+                    switch (operation.charAt(0)) {
+                        case 'g':
+                        case 'G':
+                            filename = st.nextToken();
+                            sendFile(filename);
+                            break;
+                        case 'd':
+                        case 'D':
+                            File cur_directory = new File(".");
+                            String files = getFiles(cur_directory);
+                            System.out.println("Sending file names to client");
+                            sendMessage(files);
+                            break;
+                        case 'u':
+                        case 'U':
+                            receiveFile(st.nextToken());
+                            break;
+                        default:
+                            System.out.println("Invalid Input");
+                    }
+                }
+            } catch (IOException i) {
+                System.out.println(i);
+            }
+        }
+
+        public void run() {
+            try {
+                out = new DataOutputStream(
+                        new BufferedOutputStream(socket.getOutputStream())
+                );
+                out.flush();
+                in = new DataInputStream(
+                        new BufferedInputStream(socket.getInputStream())
+                );
+                out_obj = new ObjectOutputStream(socket.getOutputStream());
+                performAuthentication();
+                performOperation();
+            } catch (IOException i) {
+                System.out.println(i);
+            } finally {
+                System.out.println("Closing connection");
+                try {
+                    in.close();
+                    out.close();
+                } catch (IOException ex) {
+                    System.out.println(ex);
+                }
+            }
+        }
     }
 }
